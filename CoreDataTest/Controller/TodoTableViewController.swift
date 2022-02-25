cea67c23850348cc795fe171f4ee9e43e328dbd8
tourbillion42 <tourbillion42@gmail.com>
2022-02-26 00:37:07 +0900

    add todoTableViewController

--- CoreDataTest/Controller/TodoTableViewController.swift
+++ CoreDataTest/Controller/TodoTableViewController.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoTableViewController: UITableViewController {
+    
+    
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
     }
     
 }
