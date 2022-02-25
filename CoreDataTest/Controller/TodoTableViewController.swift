b9fdb5e9defbd6a32401f313c0e4516fd59a96b9
tourbillion42 <tourbillion42@gmail.com>
2022-02-26 00:25:31 +0900

    add TodoTableViewController

---
+++ CoreDataTest/Controller/TodoTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TodoTableViewController.swift
+//  CoreDataTest
+//
+//  Created by Hwang on 2022/02/26.
+//
+
+import UIKit
+
+class TodoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
