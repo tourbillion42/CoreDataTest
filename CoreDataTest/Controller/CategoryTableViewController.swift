1105c79132713d7b6d02c2c4632622c1c4505da2
tourbillion42 <tourbillion42@gmail.com>
2022-02-25 23:59:21 +0900

    add cellForRowAt, numberOfRowsInSection

--- CoreDataTest/Controller/CategoryTableViewController.swift
+++ CoreDataTest/Controller/CategoryTableViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class CategoryTableViewController: UITableViewController {
-
+    
+    var categories = [Catego]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
     }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = categories[indexPath.row].title
+        cell.contentConfiguration = content
+        
+        return cell
+    }
 }
