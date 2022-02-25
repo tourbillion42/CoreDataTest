030cb8b62deb6fa403f3ba632d82dd1ad9b2318e
tourbillion42 <tourbillion42@gmail.com>
2022-02-25 23:51:37 +0900

    add first

---
+++ CoreDataTest/Controller/CategoryTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryTableViewController.swift
+//  CoreDataTest
+//
+//  Created by Hwang on 2022/02/25.
+//
+
+import UIKit
+
+class CategoryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
